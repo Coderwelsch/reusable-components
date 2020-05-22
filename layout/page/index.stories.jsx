89e172b22244ed55534df5bb693e81beed96c1c2
Your Name <social@coderwelsch.com>
2020-05-22 15:32:40 +0200

    Added storybook

---
+++ layout/page/index.stories.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Page from "./";
+
+export default {
+  title: 'Layout / Page',
+  component: Page,
+};
+
+export const Default = () => 
+	<Page title={ "Test" }></Page>;
